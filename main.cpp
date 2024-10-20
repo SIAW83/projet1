53461792ea0f255411cf118e85f207222f437297
SEFIANE ET-TAALABI <ettaalabi.sefiane@etu.uae.ac.ma>
2024-10-20 18:51:01 +0100

    Add files via upload
    le nombre de paramétre n'est pas specifie , il depant de la mémoire vive de la machine RAM

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+void para(){
+int a=0;
+while(1==1){
+long T[100000];
+ cout<<"parametre nbr :"<<a<<endl;
+ a++;
+}
+}
+int main() {
+para();
+    return 0;
+}
